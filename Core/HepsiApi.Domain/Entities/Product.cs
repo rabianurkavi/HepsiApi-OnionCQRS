3db3bb86e6fbdbfa5a1e17f1f4c982b47c4a8e1a
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-08-10 13:57:55 +0300

    Temel entityler oluşturuldu.

---
+++ Core/HepsiApi.Domain/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using HepsiApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Domain.Entities
+{
+    public class Product:EntityBase
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required int BrandId { get; set; }
+        public required decimal Price { get; set; }
+        public required decimal Discount { get; set; } //indirim uygulamak için 
+
+        public Brand Brand { get; set; }
+
+        public ICollection<Category> Categories { get; set; }
+        //public required string ImagePath { get; set; }
+    }
+}
