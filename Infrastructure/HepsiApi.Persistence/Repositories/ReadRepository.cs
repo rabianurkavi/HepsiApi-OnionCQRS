a4f24860cc5b6c1895277e5b9bae9e2a2f1bcbcb
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-08-11 15:22:33 +0300

    ReadRepository CQRS

---
+++ Infrastructure/HepsiApi.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,75 @@
+﻿using HepsiApi.Application.Interfaces.Repositories;
+using HepsiApi.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Persistence.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : class, IEntityBase, new()
+    {
+        private readonly DbContext dbContext;
+
+        public ReadRepository(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        private DbSet<T> Table { get => dbContext.Set<T>(); }
+
+        public async Task<IList<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, bool enableTracking = false)
+        {
+            /* Biz onu ne olarak vereceğimizi daha belirtmedik bu yüzden bu bir sorgu işleme geçmemiş bir sorgu ToList dediğimizde veritabanında istediğimiz yapı olarak geliyor */
+            IQueryable<T> queryable = Table;
+            if (!enableTracking) queryable = queryable.AsNoTracking();
+            /* ef core da yaptığımız sorgu sonucunda efcore takip ediyor. update işlemi gerçekleştireceğiz update edeceğimiz entityinin idsini bulduk bu entitynin update edeceğimiz özellerini belirledik ve saveasync i çağırdık
+            * bu saveasync çağırılmış olana dek tüm işlemlerde tracking mekanizması işleme giriyor ve çağırdığımız yapıdaki yapıda üzerinde bir değişiklik yapıp yapmadığımızı bulabiliyor. Burada sadece bir okuma işleme yapacağım için
+            * tracking işlemini devre bırakayım ki daha performanslı işlem olsun. Efcore bunu yaparken bellekte bir yer tutuyor. Bu yüzden tasarruf yani performansımızı arttırmış oluyoruz.
+                                                                      */
+            if (include != null) queryable = include(queryable);
+            if (predicate != null) queryable = queryable.Where(predicate);
+            if (orderBy != null)
+                return await orderBy(queryable).ToListAsync();
+            return await queryable.ToListAsync();
+        }
+        public async Task<IList<T>> GetAllByPagingAsync(Expression<Func<T, bool>>? predicate = null, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, bool enableTracking = false, int currentPage = 1, int pageSize = 3)
+        {
+            IQueryable<T> queryable = Table;
+            if (!enableTracking) queryable = queryable.AsNoTracking();
+            if (include != null) queryable = include(queryable);
+            if (predicate != null) queryable = queryable.Where(predicate);
+            if (orderBy != null)
+                return await orderBy(queryable).Skip((currentPage - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return await queryable.Skip((currentPage - 1) * pageSize).Take(pageSize).ToListAsync();
+        }
+        public async Task<T> GetAsync(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null, bool enableTracking = false)
+        {
+            IQueryable<T> queryable = Table;
+            if (!enableTracking) queryable = queryable.AsNoTracking();
+            if (include != null) queryable = include(queryable);
+
+            //queryable.Where(predicate);
+
+            return await queryable.FirstOrDefaultAsync(predicate);
+        }
+        public async Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null)
+        {
+            Table.AsNoTracking();
+            if (predicate != null) Table.Where(predicate);
+
+            return await Table.CountAsync();
+        }
+
+        public IQueryable<T> Find(Expression<Func<T, bool>> predicate, bool enableTracking=false)
+        {
+            if (!enableTracking) Table.AsNoTracking();
+            return Table.Where(predicate);
+        }
+
+    }
+}
