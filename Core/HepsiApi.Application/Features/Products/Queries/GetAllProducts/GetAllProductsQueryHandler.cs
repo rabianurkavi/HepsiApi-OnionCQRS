c55b429fbd12094e8bf7c74aeb6e1e7bd66504f3
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-08-13 19:50:43 +0300

    Mediatr

---
+++ Core/HepsiApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using HepsiApi.Application.Interfaces.UnitOfWorks;
+using HepsiApi.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Application.Features.Products.Queries.GetAllProducts
+{
+    public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQueryRequest, IList<GetAllProductsQueryResponse>>
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public GetAllProductsQueryHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public async Task<IList<GetAllProductsQueryResponse>> Handle(GetAllProductsQueryRequest request, CancellationToken cancellationToken)
+        {
+            var products = await unitOfWork.GetReadRepository<Product>().GetAllAsync();
+
+            List<GetAllProductsQueryResponse> response = new();
+            foreach (var product in products) 
+                response.Add(new GetAllProductsQueryResponse
+                {
+                    Title = product.Title,
+                    Description = product.Description,
+                    Discount = product.Discount,
+                    Price = product.Price -(product.Price * product.Discount/100),
+                });
+            return response;
+
+        }
+    }
+}
