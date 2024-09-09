8d262f692722d16b53c834cc2f7f915bde203544
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-09-09 11:29:03 +0300

    BaseRules and Configuration

--- Core/HepsiApi.Application/Exceptions/ExceptionMiddleware.cs
+++ Core/HepsiApi.Application/Exceptions/ExceptionMiddleware.cs
@@ -46,8 +46,7 @@ namespace HepsiApi.Application.Exceptions
 
             List<string> errors = new()
             {
-                $"Hata Mesajı: {exception.Message}",
-                $"Mesaj Açıklaması: {exception.InnerException?.ToString()}"
+                $"Hata Mesajı: {exception.Message}"
             };
 
             return httpContext.Response.WriteAsync(new ExceptionModel
