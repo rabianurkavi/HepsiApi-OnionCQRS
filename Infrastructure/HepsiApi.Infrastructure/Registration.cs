443e926406aa42539e4d311f1a35e3704aab42a3
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-09-13 16:20:20 +0300

    JWT Access ve Refresh Token Servisi

---
+++ Infrastructure/HepsiApi.Infrastructure/Registration.cs
@@ -0,0 +1,20 @@
+﻿using HepsiApi.Infrastructure.Tokens;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Infrastructure
+{
+    public static class Registration
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<TokenSettings>(configuration.GetSection("JWT"));
+        }
+
+    }
+}
---
+++ Infrastructure/HepsiApi.Infrastructure/Tokens/TokenService.cs
@@ -0,0 +1,70 @@
+﻿using HepsiApi.Application.Interfaces.Tokens;
+using HepsiApi.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Infrastructure.Tokens
+{
+    public class TokenService : ITokenService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly TokenSettings _tokenSettings;
+        public TokenService(IOptions<TokenSettings> options, UserManager<User> userManager)
+        {
+            _tokenSettings= options.Value;
+            _userManager= userManager;
+        }
+        public async Task<JwtSecurityToken> CreateToken(User user, IList<string> roles)
+        {
+
+            //hem uygulamaya girişimiz için claim lazım authentitation işlemleri için lazım olacak
+            //
+            var claims = new List<Claim>()
+            {
+                //jti bu claimnames tarafındaki jwt idimizi temsik ediyor
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                //jwt id ile beraber 
+                //sistemimize giriş yapan kişilerin id sini göreceğiz bununla birlikte hem jwt idsi ile beraber tokenımızın güvenliğini arttıracağız
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email)
+            };
+
+            foreach(var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role,role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_tokenSettings.Secret));
+
+            var token= new JwtSecurityToken(
+                issuer: _tokenSettings.Issuer,
+                audience: _tokenSettings.Audience,
+                expires: DateTime.Now.AddMinutes(_tokenSettings.TokenValidityInMinutes),
+                claims: claims,
+                signingCredentials: new SigningCredentials(key,SecurityAlgorithms.HmacSha256));
+
+            await _userManager.AddClaimsAsync(user, claims);
+
+            return token;
+        }
+
+        public string GenerateRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpiredToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
