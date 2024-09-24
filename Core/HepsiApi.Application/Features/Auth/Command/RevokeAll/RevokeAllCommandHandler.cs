39db61474e59c2a116a2106e1a0ff4eea907e573
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-09-24 13:57:38 +0300

    Mediatr ve JWT ile Logout İşlemleri, Revoke

---
+++ Core/HepsiApi.Application/Features/Auth/Command/RevokeAll/RevokeAllCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using HepsiApi.Application.Bases;
+using HepsiApi.Application.Interfaces.AutoMapper;
+using HepsiApi.Application.Interfaces.UnitOfWorks;
+using HepsiApi.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Application.Features.Auth.Command.RevokeAll
+{
+    public class RevokeAllCommandHandler : BaseHandler, IRequestHandler<RevokeAllCommandRequest, Unit>
+    {
+        private readonly UserManager<User> userManager;
+
+        public RevokeAllCommandHandler(UserManager<User> userManager, IMapperApp mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<Unit> Handle(RevokeAllCommandRequest request, CancellationToken cancellationToken)
+        {
+            List<User> users = await userManager.Users.ToListAsync(cancellationToken);
+            foreach (User user in users)
+            {
+                user.RefreshToken = null;
+                await userManager.UpdateAsync(user); 
+            }
+            return Unit.Value;
+        }
+    }
+}
