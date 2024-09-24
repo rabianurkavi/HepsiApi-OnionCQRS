c7912eda78d151e2222b9a1b8a95f7dfd40b874c
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-09-24 15:31:26 +0300

    Redis Connection, RedisService, RedisPipelineBehavior

---
+++ Core/HepsiApi.Application/Beheviors/RedisCacheBehavior.cs
@@ -0,0 +1,43 @@
+﻿using HepsiApi.Application.Interfaces.RedisCache;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Application.Beheviors
+{
+    public class RedisCacheBehavior<TRequest,TResponse>: IPipelineBehavior<TRequest,TResponse>
+    {
+        private readonly IRedisCacheService redisCacheService;
+        public RedisCacheBehavior(IRedisCacheService redisCacheService)
+        {
+            this.redisCacheService = redisCacheService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (request is ICacheableQuery query) 
+            {
+                var cacheKey= query.CacheKey;
+
+                var cacheTime= query.CacheTime;
+
+                var cachedData= await redisCacheService.GetAsync<TResponse>(cacheKey);
+
+                if (cachedData is not null)
+                    return cachedData;
+
+                var response = await next();
+
+                if(response is not null)
+                    await redisCacheService.SetAsync(cacheKey, response, DateTime.Now.AddMinutes(cacheTime));
+
+                return response;
+                
+            }
+            return await next();
+        }
+    }
+}
---
+++ Core/HepsiApi.Application/Interfaces/RedisCache/IRedisCacheService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Application.Interfaces.RedisCache
+{
+    public interface IRedisCacheService
+    {
+        Task<T> GetAsync<T>(string key);
+        //cache in ne kadar süre kalacağını gösteren değer=Expirationtime
+        Task SetAsync<T>(string key,T value,DateTime? expirationTime=null);
+    }
+}
